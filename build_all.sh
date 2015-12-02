079ed8f5c4dbe38bb00bd9d9020365f9b081c216
Florent Vilmart <florentvilmart@me.com>
2015-12-02 13:32:47 -0500

    Force set DTXCode

--- build_all.sh
+++ build_all.sh
@@ -2,12 +2,14 @@
 
 DIR=$(pwd)
 
+CARTHAGE_IOS=Carthage/Build/iOS
+
 git submodule update --init --recursive
 
-mkdir -p Carthage/Build/iOS
+mkdir -p ${CARTHAGE_IOS}
 
 # cleanup the directory
-rm -rf Carthage/Build/iOS/*
+rm -rf ${CARTHAGE_IOS}/*
 
 echo "travis_fold:start:Parse-SDK-iOS-OSX\r"
 
@@ -19,17 +21,18 @@ bundle exec rake package:framework:ios_dynamic
 cd $DIR
 
 # # # Copy the Parse SDK + Bolts
-unzip Parse-SDK-iOS-OSX/build/release/Parse-iOS-Dynamic.zip -d Carthage/Build/iOS
+unzip Parse-SDK-iOS-OSX/build/release/Parse-iOS-Dynamic.zip -d ${CARTHAGE_IOS}
 
 echo "travis_fold:end:Parse-SDK-iOS-OSX\r"
 
-export XCODE_XCCONFIG_FILE=$(pwd)/Configurations/iOS-module.xcconfig
-
 echo "travis_fold:start:Facebook-SDK-iOS\r"
+
+export XCODE_XCCONFIG_FILE=$(pwd)/Configurations/iOS-module.xcconfig
 # build the Facebook iOS toolchain
 facebook-ios-sdk/scripts/build_framework.sh -n -c Release
 
 unset XCCODE_XCCONFIG_FILE
+
 echo "travis_fold:end:Facebook-SDK-iOS\r"
 # copy the FBSDK frameworks
 
@@ -54,18 +57,23 @@ setVersion $FB_SDK_LOGIN_KIT_VERSION Carthage/Build/iOS/FBSDKShareKit.framework/
 rm -rf ParseFacebookUtils-iOS/Vendor/*.framework
 rm -rf ParseTwitterUtils-iOS/Vendor/*.framework
 
-cp -R Carthage/Build/iOS/* ParseFacebookUtils-iOS/Vendor/
-cp -R Carthage/Build/iOS/* ParseTwitterUtils-iOS/Vendor/
+cp -R ${CARTHAGE_IOS}/* ParseFacebookUtils-iOS/Vendor/
+cp -R ${CARTHAGE_IOS}/* ParseTwitterUtils-iOS/Vendor/
 
 echo "travis_fold:start:ParseTwitterUtils-iOS\r"
 sh Scripts/build_framework.sh ParseTwitterUtils-iOS ParseTwitterUtils ParseTwitterUtils-iOS ParseTwitterUtils
-cp -R ParseTwitterUtils-iOS/build/ios/ParseTwitterUtils.framework Carthage/Build/iOS/
+cp -R ParseTwitterUtils-iOS/build/ios/ParseTwitterUtils.framework ${CARTHAGE_IOS}/
 echo "travis_fold:end:ParseTwitterUtils-iOS\r"
 
 echo "travis_fold:start:ParseFacebookUtils-iOS\r"
 sh Scripts/build_framework.sh ParseFacebookUtils-iOS ParseFacebookUtils ParseFacebookUtilsV4-iOS ParseFacebookUtilsV4
-cp -R ParseFacebookUtils-iOS/build/ios/ParseFacebookUtilsV4.framework Carthage/Build/iOS/
+cp -R ParseFacebookUtils-iOS/build/ios/ParseFacebookUtilsV4.framework ${CARTHAGE_IOS}/
 echo "travis_fold:end:ParseFacebookUtils-iOS\r"
+
+for i in $(ls $CARTHAGE_IOS); do
+	$(/usr/libexec/PlistBuddy $CARTHAGE_IOS/$i/Info.plist -c "Set:DTXcode 0711")
+	$(/usr/libexec/PlistBuddy $CARTHAGE_IOS/$i/Info.plist -c "Set:DTXcodeBuild 7B1005")
+done
 
 zip -r ParseKit.framework.zip Carthage
 
